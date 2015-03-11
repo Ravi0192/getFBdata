d0bb10ca63d342ebc86f9d3f490ba3f2eac45e51
Diego B. Correa <ddumst@gmail.com>
2015-03-11 10:47:42 -0500

    Clean app.js code

--- js/app.js
+++ js/app.js
@@ -23,26 +23,18 @@ function checkLoginState() {
 function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
     //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
     var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
-    
     var CSV = '';    
-    //Set Report title in first row or line
     
     CSV += ReportTitle + '\r\n\n';
 
-    //This condition will generate the Label/Header
     if (ShowLabel) {
         var row = "";
-        
-        //This loop will extract the label from 1st index of on array
+
         for (var index in arrData[0]) {
-            
-            //Now convert each value to string and comma-seprated
             row += index + ',';
         }
 
         row = row.slice(0, -1);
-        
-        //append Label row with line break
         CSV += row + '\r\n';
     }
     
@@ -50,7 +42,6 @@ function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
     for (var i = 0; i < arrData.length; i++) {
         var row = "";
         
-        //2nd loop will extract each column and convert it in string comma-seprated
         for (var index in arrData[i]) {
             row += '"' + arrData[i][index] + '",';
         }
@@ -68,18 +59,9 @@ function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
     
     //Generate a file name
     var fileName = "Reporte_";
-    //this will remove the blank-spaces from the title and replace it with an underscore
     fileName += ReportTitle.replace(/ /g,"_");   
     
-    //Initialize file format you want csv or xls
     var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
-    
-    // Now the little tricky part.
-    // you can use either>> window.open(uri);
-    // but this will not work in some browsers
-    // or you will not get the correct file extension    
-    
-    //this trick will generate a temp <a /> tag
     var link = document.createElement("a");    
     link.href = uri;
     
